ea1e6ae723e4ce6fddbbb3e2f073e8b771b71fb1
Allan Silva <allanalves1511@gmail.com>
2021-05-07 17:45:12 -0300

    Criação da dimensão Customer

---
+++ models/marts/dim_customer.sql
@@ -0,0 +1,62 @@
+with selCustomer as (
+
+    select
+        customerid
+       , personid		
+       , rowguid
+    --  , storeid
+    --  , territoryid
+    -- , modifieddate      
+    from {{ ref('stg_customer') }} 
+ 
+),
+
+selPerson as (
+select 
+        businessentityid	-- PK, FK1
+      , rowguid	        -- FK Customer
+      , firstname	
+      , middlename	
+      , lastname	
+      , title	
+      , persontype		
+      , namestyle	
+     -- , suffix		
+     -- , modifieddate		
+      , emailpromotion
+      from {{ ref('stg_person') }} 
+
+), customer_with_person as (
+    
+    select
+        selCustomer.customerid
+      , selCustomer.personid		
+     -- , selCustomer.rowguid
+     -- , selCustomer.storeid
+     -- , selCustomer.territoryid
+     -- , selCustomer.modifieddate   
+      , selPerson.businessentityid	-- PK, FK1
+     -- , selPerson.rowguid	        -- FK Customer
+      , selPerson.firstname	
+     -- , selPerson.middlename	
+      , selPerson.lastname	
+      , selPerson.title	
+      , selPerson.persontype		
+      , selPerson.namestyle	
+     -- , selPerson.suffix		
+     -- , selPerson.modifieddate		
+      , selPerson.emailpromotion
+    from selCustomer
+      left join selPerson on selPerson.businessentityid= selCustomer.customerid
+   
+
+), transformed as (
+
+        select
+          row_number() over (order by customerID) as customerFK
+          , *
+          from customer_with_person
+
+)
+
+select  * from transformed
